4922e72572d0307565d13e8d3c10da3dd3278e6c
lachimolala2628 <aayushkumar1228@gmail.com>
2024-04-28 17:56:56 +0530

    initial commit for dashboard ui

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Topbar from './scenes/global/Topbar';
+import SideBar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+import Team from './scenes/team';
+import Contacts from './scenes/contacts';
+import Invoices from './scenes/invoices';
+import Form from './scenes/form';
+import Calendar from './scenes/calendar';
+import Bar from './scenes/bar';
+import Pie from './scenes/pie';
+import Line from './scenes/line';
+import Geography from './scenes/geography';
+
+const App = () => {
+
+  const [theme, colorMode] = useMode();
+
+  return (
+    <>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            <SideBar />
+            <main className="content">
+              <Topbar />
+              <Routes>
+                <Route path='/' element={<Dashboard />} />
+                <Route path='/team' element={<Team />} />
+                <Route path='/contacts' element={<Contacts />} />
+                <Route path='/invoices' element={<Invoices />} />
+                <Route path='/form' element={<Form />} />
+                <Route path='/calendar' element={<Calendar />} />
+                <Route path='/bar' element={<Bar />} />
+                <Route path='/pie' element={<Pie />} />
+                <Route path='/line' element={<Line />} />
+                <Route path='/geography' element={<Geography />} />
+              </Routes>
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </>
+  );
+}
+
+export default App
---
+++ src/scenes/pie/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PieChart from '../../components/PieChart'
+import Header from '../../components/Header'
+import { Box } from '@mui/material'
+
+
+const Pie = () => {
+    return (
+        <>
+            <Box m='20px'>
+                <Header title='PIE CHART' subtitle='Simple Pie Chart'/>
+                <Box height='75vh'>
+                    <PieChart />
+                </Box> 
+            </Box>
+        </>
+    )
+}
+
+export default Pie
